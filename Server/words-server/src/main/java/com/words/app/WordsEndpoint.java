f8b5ffaf26022de56fd5ad944722e4df3a4ad5d6
Richardyang510 <richyang510@gmail.com>
2019-01-27 02:46:46 -0500

    Add vision api endpoint

--- Server/words-server/src/main/java/com/words/app/WordsEndpoint.java
+++ Server/words-server/src/main/java/com/words/app/WordsEndpoint.java
@@ -1,9 +1,17 @@
 package com.words.app;
 
+import com.google.cloud.vision.v1.*;
+import com.google.protobuf.ByteString;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/words")
 public class WordsEndpoint {
@@ -12,9 +20,47 @@ public class WordsEndpoint {
     }
 
     @POST
-    @Path("todo")
+    @Path("recog")
     @Produces(MediaType.APPLICATION_JSON)
-    public void initializeDB() {
+    public void recognize() {
         System.out.println("post request called");
+        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+            // The path to the image file to annotate
+            String fileName = "resource/s-l300.jpg.jpg";
+            java.nio.file.Path path = Paths.get(fileName);
+            byte[] data = Files.readAllBytes(path);
+            ByteString imgBytes = ByteString.copyFrom(data);
+
+            // Builds the image annotation request
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+            Image img = Image.newBuilder().setContent(imgBytes).build();
+            Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
+            AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+                    .addFeatures(feat)
+                    .setImage(img)
+                    .build();
+            requests.add(request);
+
+            // Performs label detection on the image file
+            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.printf("Error: %s\n", res.getError().getMessage());
+                    return;
+                }
+
+                for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+                    annotation.getAllFields().forEach((k, v) ->
+                            System.out.printf("%s : %s\n", k, v.toString()));
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
